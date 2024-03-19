9f3850732dbcc222d7805c2a941ade1daa1e40bc
heitor-sanches <150952274+heitor-sanches@users.noreply.github.com>
2024-03-19 00:06:58 +0000

    hs10

--- api.test.js
+++ api.test.js
@@ -5,6 +5,11 @@ describe('Teste GET /users', () => {
   it('deve responder com status 200', async () => {
     const response = await supertest(app).get('/users');
     expect(response.statusCode).toBe(200);
+
+    expect(response.body).toBeInstanceOf(Array);
+    response.body.forEach(element =>{
+      expect(element).toHaveProperty("name");
+    });
   });
 });
 
